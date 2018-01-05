34074aad13ef978983e695a713f9fc7da062a616
Keyur Parikh <Keyur.Parikh@arup.com>
2018-01-05 10:47:55 +0000

    First Commit

---
+++ src/webparts/carouselWp/ICarouselWPWebPartProps.ts
@@ -0,0 +1,23 @@
+export interface ICarouselWPWebPartProps {
+  description: string;
+  speed: number;
+  showButton: boolean;
+  showTitle: boolean;
+  autoPlay: boolean;
+  autoPlayDelay: number;
+  bringToFront: boolean;
+  farScale: number;
+  mirrorGap: number;
+  mirrorHeight: number;
+  mirrorOpacity: number;
+  yOrigin: number;
+  yRadius: number;
+  xOrigin: number;
+  xRadius: number;
+  items: any[];
+  font: string;
+  fontSize: string;
+  fontColor: string;
+  height: number;
+  itemHeight: number;
+}
